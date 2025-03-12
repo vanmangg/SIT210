2433a6c9caac422fe612eac5a8269aa339146172
Van Mang <147131231+vanmangg@users.noreply.github.com>
2025-03-12 20:36:14 +1100

    Add files via upload

---
+++ Task 2.1P/WriteMultipleFields/WriteMultipleFields.ino
@@ -0,0 +1,101 @@
+#include "WiFiNINA.h"
+#include "secrets.h"
+#include "ThingSpeak.h" 
+#include "DHT.h"
+#define DHTPIN 2 
+#define DHTTYPE DHT22 
+
+DHT dht(DHTPIN, DHTTYPE);
+char ssid[] = SECRET_SSID;  
+char pass[] = SECRET_PASS;   
+int keyIndex = 0;            
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+// Initialize our values
+int number1 = 0;
+int number2 = 0;
+
+String myStatus = "";
+
+void setup() {
+  Serial.begin(115200);      // Initialize serial 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+
+  dht.begin();
+  
+  ThingSpeak.begin(client);  // Initialize ThingSpeak 
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+
+
+
+  // figure out the status message
+  if(number1 > number2){
+    myStatus = String("field1 is greater than field2"); 
+  }
+  else if(number1 < number2){
+    myStatus = String("field1 is less than field2");
+  }
+  else{
+    myStatus = String("field1 equals field2");
+  }
+  
+  // set the status
+  ThingSpeak.setStatus(myStatus);
+  
+  // write to the ThingSpeak channel 
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  
+  delay(20000); // Wait 20 seconds to update the channel again
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));  
+
+  ThingSpeak.setField(1, h);
+  ThingSpeak.setField(2, t);
+}
